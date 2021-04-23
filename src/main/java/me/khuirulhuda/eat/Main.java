aaaa54929de9614810ecf81df86e8dfafc449d50
Khuirul-Huda <hudah2102@gmail.com>
2021-04-23 12:39:52 +0700

    update

---
+++ src/main/java/me/khuirulhuda/eat/Main.java
@@ -0,0 +1,16 @@
+package com.khuirulhuda.eat;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Plugin Enabled");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("Plugin disabled");
+    }
+}

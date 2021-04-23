3119e886a023d898e3ad5048efa743e9fede1554
Khuirul-Huda <hudah2102@gmail.com>
2021-04-23 14:07:31 +0700

    update

--- src/main/java/me/khuirulhuda/eat/listener/EatCommand.java
+++ src/main/java/me/khuirulhuda/eat/listener/EatCommand.java
@@ -13,7 +13,7 @@ import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.command;
+import org.bukkit.*;
 
 
 

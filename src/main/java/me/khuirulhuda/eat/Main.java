0ad97e445ed4e63024baea7645cab94ae982081c
Khuirul-Huda <hudah2102@gmail.com>
2021-05-11 05:08:00 +0700

    add new directory

--- src/main/java/me/khuirulhuda/eat/Main.java
+++ src/main/java/me/khuirulhuda/eat/Main.java
@@ -3,7 +3,7 @@ package me.khuirulhuda.eat;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.net.*;
 import org.bukkit.event.Listener;
-import me.khuirulhuda.eat.Metrics;
+import org.bstats.bukkit.Metrics;
 import me.khuirulhuda.eat.listener.*;
 
 

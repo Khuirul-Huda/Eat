6ccdcb251ae81c2ace21cba6b4c3345666fe9298
Khuirul-Huda <hudah2102@gmail.com>
2021-05-11 04:52:00 +0700

    update pom.xml

--- src/main/java/me/khuirulhuda/eat/Main.java
+++ src/main/java/me/khuirulhuda/eat/Main.java
@@ -3,7 +3,7 @@ package me.khuirulhuda.eat;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.net.*;
 import org.bukkit.event.Listener;
-import org.bstats.bukkit.Metrics;
+import me.khuirulhuda.eat.org.bstats.*;
 import me.khuirulhuda.eat.listener.*;
 
 
